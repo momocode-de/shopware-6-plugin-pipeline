8c8bdb3054aaf00352d8356b609f7347daabb111
Moritz Müller <moritz.mueller@interlutions.de>
2022-10-15 15:18:02 +0200

    Removes node version action and adds downgrade of npm

--- .github/actions/shopware-6-plugin-action/scripts/init-action.sh
+++ .github/actions/shopware-6-plugin-action/scripts/init-action.sh
@@ -1,5 +1,7 @@
 apt update && apt install -y jq
+npm install -g 8.3.2
 npm config set legacy-peer-deps true
+export CI=1
 export SHOPWARE_SKIP_BUNDLE_DUMP=1
 export SHOPWARE_SKIP_FEATURE_DUMP=1
 export SHOPWARE_SKIP_ASSET_COPY=1
