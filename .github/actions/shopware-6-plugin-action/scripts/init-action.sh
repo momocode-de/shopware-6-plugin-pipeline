d1fa48678f16b64814d04278a24bdec94cbc7a8d
Moritz Müller <moritz.mueller@interlutions.de>
2022-10-15 12:09:10 +0200

    Adds definition of some environment variables

--- .github/actions/shopware-6-plugin-action/scripts/init-action.sh
+++ .github/actions/shopware-6-plugin-action/scripts/init-action.sh
@@ -1,2 +1,9 @@
-apt update
-apt install -y jq
+apt update && apt install -y jq
+npm config set legacy-peer-deps true
+export CI=1
+export SHOPWARE_SKIP_BUNDLE_DUMP=1
+export SHOPWARE_SKIP_FEATURE_DUMP=1
+export SHOPWARE_SKIP_ASSET_COPY=1
+export SHOPWARE_SKIP_THEME_COMPILE=1
+export DISABLE_ADMIN_COMPILATION_TYPECHECK=1
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
