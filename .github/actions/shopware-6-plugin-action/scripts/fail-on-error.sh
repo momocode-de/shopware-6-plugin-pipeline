a6ff662a27fe45d1434f6f82b4a18f8e17ae0537
Moritz Müller <moritz.mueller@interlutions.de>
2022-10-15 11:20:07 +0200

    Fix fail-on-error script and improves job name

--- .github/actions/shopware-6-plugin-action/scripts/fail-on-error.sh
+++ .github/actions/shopware-6-plugin-action/scripts/fail-on-error.sh
@@ -1,6 +1,8 @@
-CYPRESS_FAILURES=$(cat cypress-report/index.json | jq '.stats.failures')
+if [ -f "cypress-report/index.json" ]; then
+    CYPRESS_FAILURES=$(cat cypress-report/index.json | jq '.stats.failures')
 
-if [ "${CYPRESS_FAILURES}" != "0" ]; then
-    echo "At least one cypress test failed!"
-    exit 1
+    if [ "${CYPRESS_FAILURES}" != "0" ]; then
+        echo "At least one cypress test failed!"
+        exit 1
+    fi
 fi
